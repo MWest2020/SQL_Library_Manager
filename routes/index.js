26b91966a8f28e5d7eda3a9021ab7787f9af73bc
MWest2020 <mwesterweel@hotmail.com>
2021-01-16 00:42:26 +0100

    database and routes hooked up

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-//STEP 6
-const Book = require('../models').Book
-
-function asyncHandler(cb){
-  return async(req, res, next) => {
-    try {
-      await cb(req, res, next);
-    } catch(error){
-      next(error);
-    }
-  }
-}
-
-
 /* GET home page. */
-router.get('/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll();
-  // console.log(books);
-  res.render( books.map(book => book.toJSON()) );
-  console.log(books);
-}));
-
+router.get('/', (req, res, next) => {
+  res.redirect('/books');
+});
 
 module.exports = router;
